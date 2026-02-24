8680c8092e991caec695e20dc2b526e4b7bef01e
XDsiroriginal <anna.sawhenko1984@gmail.com>
2026-02-24 15:08:02 +0700

    create ProductCatalog.vue and get api response

---
+++ my-app/src/components/ProductCatalog.vue
@@ -0,0 +1,29 @@
+<script>
+
+  import axios from 'axios'
+
+  export default {
+    name: 'ProductCatalog',
+
+    async mounted() {
+      try {
+        const response = await axios.get(`${process.env.VUE_APP_API_URL}/products`)
+        console.log(response.data)
+      } catch (error) {
+        console.log('ошибка отправки get запроса на сервер')
+      }
+    }
+  }
+
+
+</script>
+
+<template>
+
+  <h1>console test</h1>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-app/src/views/home.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+  import { ref, onMounted } from 'vue'
+  import ProductCatalog from "@/components/ProductCatalog.vue";
+
+</script>
+
+<template>
+
+  <ProductCatalog></ProductCatalog>
+
+</template>
+
+<style scoped>
+
+</style>
